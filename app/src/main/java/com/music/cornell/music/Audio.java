0a6ced74d4e4ed72a08b172ec55c6e667dafa536
David Burgstahler <davidburgstahler@dhcp-rhodes-574.eduroam.cornell.edu>
2016-10-14 17:54:44 -0400

    audio methods

--- app/src/main/java/com/music/cornell/music/Audio.java
+++ app/src/main/java/com/music/cornell/music/Audio.java
@@ -4,10 +4,20 @@ package com.music.cornell.music;
  * Created by davidburgstahler on 10/14/16.
  */
 
+
+
 import android.media.MediaPlayer;
 
-
 public class Audio {
+    public static MediaPlayer mediaPlayer1 = new MediaPlayer();
+    public static MediaPlayer mediaPlayer2 = new MediaPlayer();
+    
+
+    public void startSound(MediaPlayer m) {
+        m.start();
+        m.setVolume(0,0);
+    }
+
     public void fadeIn(MediaPlayer m) {
         float n = 0;
         while(n <= 1) {
