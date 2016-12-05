88c95edc0738ced3b862b78fc9758d0fd2046bc2
Daniel Weber <dantech88@yahoo.com>
2016-12-05 16:13:11 -0500

    added media factory

---
+++ app/src/main/java/com/music/cornell/music/MediaFactory.java
@@ -0,0 +1,16 @@
+package com.music.cornell.music;
+
+import android.media.MediaPlayer;
+
+/**
+ * Created by dantech on 12/5/16.
+ */
+
+public class MediaFactory {
+
+    public static MediaPlayer[] createCentralSounds() {
+        MediaPlayer[] cent = new MediaPlayer[15];
+
+        return cent;
+    }
+}
